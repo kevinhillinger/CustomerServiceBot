4aee6c213a057ac97aa97473dad76b72a08ffdf5
Kevin M. Gates <kevin@dreaddontdie.com>
2017-02-23 14:50:57 -0600

    Added resource file and strings for OrderRoot.cs

--- Crm.SampleBot/Dialogs/Order/OrderRoot.cs
+++ Crm.SampleBot/Dialogs/Order/OrderRoot.cs
@@ -4,16 +4,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
+using System.Resources;
+using System.Globalization;
 
 namespace Crm.SampleBot.Dialogs.Order
 {
     [Serializable]
     class OrderRoot : IDialog<object>
     {
+        //refer to the resource file ProjectName.Filename (minus the en-US)
+        static ResourceManager rmOrderRoot = new ResourceManager("Crm.SampleBot.Resources.OrderRoot", Assembly.GetExecutingAssembly());
+        static ResourceManager rmRootDialog = new ResourceManager("Crm.SampleBot.Resources.RootDialog", Assembly.GetExecutingAssembly());
+
+        //Set the language to be used; you can change this on-demand to change the langauage across the app
+        //You will pass this everytime you request a value from the resx file
+        static CultureInfo ciEnglish = new CultureInfo("en-US");
+
         // Options for user to choose
-        private const string OrderNumber = "Search by Order Number";
-        private const string OrderDate = "Search by Order Date";
-        private const string OrderAccount = "Search by Account Number";
+        private static string OrderNumber = rmOrderRoot.GetString("OrderNumber", ciEnglish);
+        private static string OrderDate = rmOrderRoot.GetString("OrderDate", ciEnglish);
+        private static string OrderAccount = rmOrderRoot.GetString("OrderAccount", ciEnglish);
         private readonly IDialogFactory dialogFactory;
 
         public OrderRoot(IDialogFactory dialogFactory)
@@ -27,7 +38,7 @@ namespace Crm.SampleBot.Dialogs.Order
                 context,
                 this.OnOptionSelected,
                 new List<string>() { OrderNumber, OrderDate, OrderAccount },
-                String.Format("What would you like to do?"), "Not a valid option", 0);
+                String.Format(rmRootDialog.GetString("WhatToDo", ciEnglish)), rmRootDialog.GetString("NotValid", ciEnglish), 0);
         }
 
         private async Task OnOptionSelected(IDialogContext context, IAwaitable<string> result)
@@ -36,19 +47,15 @@ namespace Crm.SampleBot.Dialogs.Order
             {
                 //capture which option then selected
                 string optionSelected = await result;
-                switch (optionSelected)
-                {
-                    case OrderNumber:
-                        context.Call(dialogFactory.Create<OrderNumber>(), this.ResumeAfterOptionDialog);
-                        break;
 
-                    case OrderAccount:
-                        context.Call(dialogFactory.Create<OrderAccount>(), this.ResumeAfterOptionDialog);
-                        break;
-
-                    case OrderDate:
-                        context.Call(dialogFactory.Create<OrderDate>(), this.ResumeAfterOptionDialog);
-                        break;
+                if (optionSelected == OrderNumber) {
+                    context.Call(dialogFactory.Create<OrderNumber>(), this.ResumeAfterOptionDialog);
+                }
+                if (optionSelected == OrderAccount) {
+                    context.Call(dialogFactory.Create<OrderAccount>(), this.ResumeAfterOptionDialog);
+                }
+                if (optionSelected == OrderDate) {
+                    context.Call(dialogFactory.Create<OrderDate>(), this.ResumeAfterOptionDialog);
                 }
             }
             catch (TooManyAttemptsException ex)
